aa0ce6276c3a9d7ba5e82add87bb44e54f7a6f23
njuno <pmnjuno@gmail.com>
2020-11-15 03:33:55 +0300

    dd

--- system/config/config.php
+++ system/config/config.php
@@ -3,15 +3,15 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-// define('DB_PASS', 'Java_2147483647');
-define('DB_PASS', '');
+define('DB_PASS', 'Java_2147483647');
+// define('DB_PASS', '');
 define('DB_NAME', 'blog');
 
 
 
 // URL ROOT
-define("URL_ROOT", "http://localhost/websites/meme/php/index.php?url=");
-// define("URL_ROOT", "index.php?url=");
+// define("URL_ROOT", "http://localhost/websites/meme/php/index.php?url=");
+define("URL_ROOT", "index.php?url=");
 
 
 // SITE NAME
