e9d43f515ac36bd30d298eeeaa3495a9a10e60ab
njuno <pmnjuno@gmail.com>
2020-11-15 04:01:33 +0300

    dd

--- system/config/config.php
+++ system/config/config.php
@@ -10,8 +10,9 @@ define('DB_NAME', 'blog');
 
 
 // URL ROOT
-// define("URL_ROOT", "http://localhost/websites/meme/php/index.php?url=");
-define("URL_ROOT", "index.php?url=");
+// define("URL_ROOT", "http://localhost/websites/meme/php/");
+// define("URL_ROOT", "index.php?url=");
+define("URL_ROOT", 'http://thejnmedia.com/');
 
 
 // SITE NAME
