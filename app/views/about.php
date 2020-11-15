1a15ac335e823bf8f993bc3ce65516adf04bb449
njuno <pmnjuno@gmail.com>
2020-11-15 06:24:31 +0300

    dd

--- app/views/about.php
+++ app/views/about.php
@@ -11,30 +11,68 @@
 	<div class="container">
 		<h2 class="title mb-3">About Me</h2>
 
-		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. </p>
-		<figure><img class="img-fluid" src="assets/images/about-me.jpg" alt="image"></figure>
+		<p>In 2015, a neighbour introduced me to programming, now I am a web and software applications developer with over 5 years’ experience developing a wide range of websites and desktop applications for window operating system for a range of clients. I have proven expertise in building hospital management applications and responsive websites. I understand the software development project lifecycle in depth and am very capable at all aspects of development from project planning to requirements gathering through to writing and testing code, creating documentation and support.  </p>
+		<figure><img class="img-fluid" src="<?= get_img('header_bg.jpg') ?>" alt="image"></figure>
 		<h5 class="mt-5">About The Blog</h5>
-		<p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.</p>
+		<p>On this blog website, I post blogs on different subject, but mostly on web and software development. I also post opinions on various aspects of our lifes like elections, agriculture and healthcare insurance and the way they can be improved by current technology. Other topics covered include udemy course rankings, geography and history of various stuffs, career as a developer, artificial intelligence, accounting among others things. Kindly note that what is posted here will be based on my opinions, so relax, is just an opinion from a nobody.</p>
 		<h5 class="mt-5">My Skills and Experiences</h5>
-		<p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
-		<h5 class="mt-5">Side Projects</h5>
-		<p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+		<section class="regions">
+            
+            <div class="container flex">
+                <div class="card p-2">
+                    <h4>
+                        C#
+                    </h4>
+                    <img src="<?= get_img('skills/csharp.png') ?>" alt="">
+                </div>
+                <div class="card p-2">
+                    <h4>
+                        GO
+                    </h4>
+                    <img src="<?= get_img('skills/go.png') ?>" alt="">
+                </div>
+                <div class="card p-2">
+                    <h4>
+                        Node Js
+                    </h4>
+                    <img src="<?= get_img('skills/node.png') ?>" alt="">
+                </div>
+                <div class="card p-2">
+                    <h4>
+                        Python
+                    </h4>
+                    <img src="<?= get_img('skills/python.png') ?>" alt="">
+                </div>
+                <div class="card p-2">
+                    <h4>
+                        Ruby
+                    </h4>
+                    <img src="<?= get_img('skills/ruby.png') ?>" alt="">
+                </div>
+                <div class="card p-2">
+                    <h4>
+                        Php
+                    </h4>
+                    <img src="<?= get_img('skills/php.png') ?>" alt="">
+                </div>
+            </div>
+        </section>
+		
+		<p>I have practical experience in all programming languages listed above. C sharp is my favourite programming laguage followed closely by Javascript (nodejs - not a programming a programming language). C sharp is what I use when making Desktop application, Nodejs is what i use for web development. I also love working with php, I find php easy to debug than other other languages.</p>
 
-		<figure><a href="https://made4dev.com"><img class="img-fluid" src="assets/images/promo-banner.jpg" alt="image"></a></figure>
+		
 	</div>
 </article><!--//about-section-->
 
 <section class="cta-section theme-bg-light py-5">
 	<div class="container text-center">
-		<h2 class="heading">Newsletter</h2>
-		<div class="intro">Welcome to my blog. Subscribe and get my latest blog post in your inbox.</div>
-		<form class="signup-form form-inline justify-content-center pt-3">
-							<div class="form-group">
-									<label class="sr-only" for="semail">Your email</label>
-									<input type="email" id="semail" name="semail1" class="form-control mr-md-1 semail" placeholder="Enter email">
-							</div>
-							<button type="submit" class="btn btn-primary">Subscribe</button>
-					</form>
+		<h2 class="heading">Updates</h2>
+		<div class="intro">Follow me on social media for updates.</div>
+		<ul class="social-list list-inline py-3 mx-auto">
+             <li class="list-inline-item"><a href="https://twitter.com/my_name_is_juno" target="_blank"><i class="fab fa-twitter fa-fw"></i></a></li>
+             <li class="list-inline-item"><a href="https://www.instagram.com/my_name_is_njuno/" target="_blank"><i class="fab fa-instagram fa-fw"></i></a></li>
+
+         </ul><!--//social-list-->
 	</div><!--//container-->
 </section>
 
